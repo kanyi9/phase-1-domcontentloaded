fd95cf9be79ef3a6fc1ac510aa593fa2e85cf52c
Peter <peterkanyi19193@gmail.com>
2024-04-07 09:37:38 +0300

    adds dom content loaded

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    var output = document.getElementById('text')
+    output.innerHTML ="This is really cool!"
+  });
